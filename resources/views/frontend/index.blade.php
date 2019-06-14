ee58de7c00d382981c5a817987cb6261d64e233d
Eber Ortiz <ortizmas14@gmail.com>
2019-06-13 23:33:30 -0300

    Atualizando tudo kkk

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-1" id="mainNav">
         <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="{{ asset('assets/img/logo/logo-branco-36x36.png') }}" alt=""></a>
+            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="{{ asset('assets/img/logo/turma-61.svg') }}" alt="Nominata" width="60"></a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -37,26 +37,32 @@
     </nav>
 
     <!-- Home section -->
-    <header class="masthead" id="home">
+
+    <div class="container-fluid h-100 p-0">
+        <div class="bg-home">
+            <img src="{{ asset('/assets/img/bg/bg-home-2.jpg') }}" alt="" class="img-fluid">
+        </div>
+    </div>
+    {{-- <header class="masthead" id="home">
         <div class="container h-100">
           <div class="row h-100 align-items-center justify-content-center text-center">
-            {{-- <div class="col-lg-10 align-self-end">
+            <div class="col-lg-10 align-self-end">
               <h1 class="text-uppercase text-white font-weight-bold">Formandos 2019</h1>
               <hr class="divider my-4">
-            </div> --}}
-            {{-- <div class="col-lg-8 align-self-baseline">
+            </div>
+            <div class="col-lg-8 align-self-baseline">
               <p class="text-white-75 font-weight-light mb-5">Start Bootstrap can help you build better websites using the Bootstrap framework! Just download a theme and start customizing, no strings attached!</p>
               <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
-            </div> --}}
+            </div>
           </div>
         </div>
-        
-    </header>
+    </header> --}}
+    <div class="clearfix"></div>
     <div class="h-divider-one"></div>
 
     <!-- Formandos Section -->
     <section id="formandos">
-        <div class="container pt-3 pb-4">
+        <div class="container pt-5 pb-5">
             <div class="row">
                 <div class="col-md-4">
                     <h3>Formandos</h3>
@@ -79,7 +85,7 @@
                     </select>
                 </div>
                 <div class="col-md-2 right-align">
-                    <a class="w-100 btn btn-dark text-white clear-filter">TODOS</a>
+                    <a class="w-100 btn btn-dark text-white clear-filter btn-red">TODOS</a>
                 </div>
             </div>
         </div>
@@ -92,7 +98,7 @@
                             <div class="bg-img" style="background-image: url({{ asset('uploads/trainees/'. $trainee->image) }});">
                                 
                             </div>
-                            <div class="formandos-box-caption bg-status-{{ $trainee->status }}">
+                            <div class="formandos-box-caption">
                               <div class="project-category text-white-50">
                                 {{ $trainee->name }}
                               </div>
@@ -101,23 +107,23 @@
                               </div>
                             </div>
                         </a>
+                        <a href="{{ url($trainee->slug) }}" style="text-decoration: none;">
                         <div class="card border-0">
-                            <a href="{{ url($trainee->slug) }}" style="text-decoration: none;">
+                            
                                 <div class="card-body bg-status-{{ $trainee->status }}">
-                                    <p class="text-white">
-                                        {{ $trainee->name }} <br>
-                                        <strong>Status: </strong>{!! getStatus($trainee->status) !!}
-                                    </p>
-                                    <span class="badge badge-light float-left text-red">{{ $trainee->association->union->initials }}</span>
-                                    <span class="badge badge-light float-right text-red">{{ $trainee->association->initials }}</span>
+                                    <span class="text-white"><strong>{{ $trainee->name }}</strong></span><br>
+
+                                    <small class="text-white float-left"><strong>Status: </strong>{!! getStatus($trainee->status) !!}</small>
+                                    <small class="text-white float-right">{{ $trainee->association->union->initials }} / {{ $trainee->association->initials }}</small>
                                     <div class="clearfix">
                                         
                                     </div>
                                     <div class="dropdown-divider"></div>
-                                    <a href="{{ url($trainee->slug) }}" class="card-link text-white">Visualizar perfil</a>
+                                    <small class="text-white">Visualizar perfil</small>
                                 </div>
-                            </a>
+                            
                         </div>
+                        </a>
                     </div>
                 @endforeach
             </div>
@@ -165,32 +171,32 @@
     <section class="page-section pb-4" id="data">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-lg-10 pl-0 pt-3 pb-4">
+                <div class="col-lg-10 pl-2 pt-3 pb-4">
                     <h3>Datas</h3>
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-md-3">
+                <div class="col-md-3 pb-3">
                     <div class="media">
-                        <img src="{{ asset('assets/img/datas/06.png') }}" class="align-self-center mr-3" width="80" alt="06 DEZ">
+                        <img src="{{ asset('assets/img/datas/06.svg') }}" class="align-self-center mr-3" width="80" alt="06 DEZ">
                         <div class="media-body">
                             <h5 class="mt-0">AS <strong>19H30</strong><br>CULTO DE <br><strong>GRATIDÃO</strong></h5>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3 pb-3">
                     <div class="media">
-                        <img src="{{ asset('assets/img/datas/07.png') }}" class="align-self-center mr-3" width="80" alt="07 DEZ">
+                        <img src="{{ asset('assets/img/datas/07.svg') }}" class="align-self-center mr-3" width="80" alt="07 DEZ">
                         <div class="media-body">
                             <h5 class="mt-0">AS <strong>9H00</strong><br>CULTO DE <br><strong>CONSAGRAÇÃO</strong></h5>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 pb-3">
                     <div class="media">
-                        <img src="{{ asset('assets/img/datas/08.png') }}" class="align-self-center mr-3" width="80" alt="08 DEZ">
+                        <img src="{{ asset('assets/img/datas/08.svg') }}" class="align-self-center mr-3" width="80" alt="08 DEZ">
                         <div class="media-body">
                             <h5 class="mt-0">AS <strong>19H30</strong><br>CERIMÔNIA DE<br><strong>COLAÇÃO DE GRAU</strong></h5>
                         </div>
@@ -199,11 +205,11 @@
             </div>
         </div>
     </section>
-    <div class="h-divider-one"></div>
+    {{-- <div class="h-divider-one"></div> --}}
 
     <!-- Services Section -->
     <section class="page-section bg-light" id="services">
-        <div class="container">
+        {{-- <div class="container">
             <div class="row no-gutters justify-content-center">
                 <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2 col-xl-2">
                     <img class="img-fluid" src="{{ asset('assets/img/diretiva/01.png') }}" alt="" width="100%">
@@ -225,11 +231,14 @@
                 </div>
             </div>
 
+        </div> --}}
+        <div class="container-fluid p-0">
+            <img src="{{ asset('assets/img/bg/comissao.jpg') }}" alt="Comissão" class="img-fluid">
         </div>
         
     </section>
-    <div class="h-divider-two"></div>
-    <section class="page-section bg-dark pt-3" style="padding-top: -20px;">
+    {{-- <div class="h-divider-two"></div> --}}
+    {{-- <section class="page-section bg-dark pt-3" style="padding-top: -20px;">
         <div class="container">
             <div class="row no-gutters justify-content-center">
                 <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2 col-xl-2">
@@ -258,30 +267,30 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> --}}
 
     <!--Section logos-->
-    <section class="page-section bg-grey pt-5 pb-4">
+    <section class="page-section bg-grey">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-sm-12 col-md-4 col-lg-4 text-left pb-3">
-                    <img src="{{ asset('assets/img/logo/fadba.svg') }}" alt="" class="img-fluid" width="250">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-sm-12 col-md-3 col-lg-3 p-5">
+                    <img src="{{ asset('assets/img/logo/logo-fadba.svg') }}" alt="" class="img-fluid" width="100%">
                 </div>
-                <div class="col-sm-12 col-md-4 col-lg-4 text-center pb-3">
-                    <img src="{{ asset('assets/img/logo/salt.svg') }}" alt="" class="img-fluid" width="250">
+                <div class="col-sm-12 col-md-3 col-lg-3 p-5">
+                    <img src="{{ asset('assets/img/logo/logo-salt.svg') }}" alt="" class="img-fluid" width="100%">
                 </div>
-                <div class="col-sm-12 col-md-4 col-lg-4 text-right pb-3">
-                    <img src="{{ asset('assets/img/logo/turma-61.svg') }}" alt="" class="img-fluid" width="95">
+                <div class="col-sm-12 col-md-3 col-lg-3 p-5">
+                    <img src="{{ asset('assets/img/logo/logo-turma-61.svg') }}" alt="" class="img-fluid" width="100%">
                 </div>
             </div>
             <div class="dropdown-divider"></div>
             <div class="row pt-3">
                 <div class="col-md">
-                    <p class="text-white address">
-                        BR-101, km 197, Capoeiruçu <br>
-                        Caixa Postal 18, Cachoeira Bahia, <br>
-                        CEP: 44.300-000 - Brasil <br>
-                        Fone: +55 (75) 3425-8000 <br>
+                    <p class="text-white address text-center">
+                        BR-101, km 197, Capoeiruçu
+                        Caixa Postal 18, Cachoeira Bahia,
+                        CEP: 44.300-000 - Brasil 
+                        Fone: +55 (75) 3425-8000
                     </p>
                 </div>
             </div>
